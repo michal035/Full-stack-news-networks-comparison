960f3895bd5d3e18cbe542b228bbfdfd580a952e
Michał Wisełka <mwiselka035@gmail.com>
2022-12-12 17:01:18 +0100

    enhanced proxy testing

--- crawling/proxy/get_proxy_servers.py
+++ crawling/proxy/get_proxy_servers.py
@@ -1,6 +1,20 @@
 import requests 
 import threading
 from queue import Queue
+import pandas as pd
+import signal
+import sys
+
+
+def interrupt_handler(signum, frame):
+    print("\n")
+    print(df)
+    print("\n")
+    sys.exit()
+
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
 
 
 list_of_working_proxies = []
@@ -14,8 +28,19 @@ with open("/home/michal/Documents/Python/scraping/test/crawling/proxy/raw_list.t
         q.put(i)
 
 
+df = pd.DataFrame({
+    'ipaddress_with_port' : [],
+    'time' : [],
+    'ipaddress' : []
+
+
+})
+
+
 def t():
     global q
+    global df
+
     while not q.empty():
         p = q.get()
         
@@ -25,9 +50,12 @@ def t():
             continue
         if res.status_code == 200:
             print(p)
+            time = res.elapsed
+            print(time)
+            df2 = pd.DataFrame ({"ipaddress_with_port": [p],'time': [time], 'ipaddress': [(p.split(":")[0])] })
+            df = pd.concat([df,df2], ignore_index=True)
 
 
 for _ in range(5):
     threading.Thread(target=t).start()
-
 
