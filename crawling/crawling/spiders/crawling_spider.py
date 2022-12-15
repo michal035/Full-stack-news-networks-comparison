8ba91db6951decba9295072f126460d56a2349a6
Michał Wisełka <mwiselka035@gmail.com>
2022-12-15 13:11:43 +0100

    added proxy rotation - works great in test file - 'crawling_spider, there are some issue when crawling on tvn site'

--- crawling/crawling/spiders/crawling_spider.py
+++ crawling/crawling/spiders/crawling_spider.py
@@ -1,8 +1,9 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy import Request
-import shutil
 import requests
+import random
+import pandas as pd
 
 
 class CrawlingSpider(CrawlSpider):
@@ -31,26 +32,34 @@ class CrawlingSpider(CrawlSpider):
 class CrawlingSpider(CrawlSpider):
     name = "spider2"
     allowed_domains = ["httpbin.org"]
-    start_urls = ["https://httpbin.org/ip"]
+    start_urls = ["http://ipinfo.io/ip"]
     
 
     rules = (
         Rule(LinkExtractor(allow="/", deny="category"), callback="parse_item"),
     )
 
-    CUSTOM_PROXY = "http://31.186.239.245:8080"
+
+
+    df = pd.read_csv('/home/michal/Documents/Python/scraping/test/crawling/proxy/working_proxies.csv', sep=" ") 
+
+    l = len(df.index)
+    random_proxy = df.iloc[random.randint(0, l-1)][0]
+
+    CUSTOM_PROXY = f"http://{random_proxy}"
 
 
     def start_requests(self):
         for url in self.start_urls:
-            re =  Request(url=url, callback=self.parse)
+            re =  Request(url=url, callback=self.parse_item)
             re.meta["proxy"] = self.CUSTOM_PROXY
+
             yield re
                    
     
     def parse_item(self, response):
         yield {
-            "ip" : response.body,
+            "IP" : response.body,
            
         }
 
