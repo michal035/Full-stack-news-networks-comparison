07b974b86c0d5b7dd862df002353015f273b1197
Michał Wisełka <mwiselka035@gmail.com>
2022-12-15 13:12:25 +0100

    added proxy rotation - works great in test file - 'crawling_spider, there are some issue when crawling on tvn site '

--- crawling/crawling/spiders/main_spider.py
+++ crawling/crawling/spiders/main_spider.py
@@ -1,5 +1,11 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy import Request
+
+import pandas as pd
+import random
+
+#In future it might be needed to outsource getting random proxy to separate file
 
 
 
@@ -12,6 +18,22 @@ class CrawlingSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow="najnowsze", deny="tvnwarszawa"), callback="parse_item"),
     )
+    
+    #I'm not sure why full path is needed - otherwise it won't work
+    
+    """df = pd.read_csv('/home/michal/Documents/Python/scraping/test/crawling/proxy/working_proxies.csv', sep=" ") 
+
+    l = len(df.index)
+    random_proxy = df.iloc[random.randint(0, l-1)][0]
+    CUSTOM_PROXY = f"http://{random_proxy}"
+
+
+    def start_requests(self):
+        for url in self.start_urls:
+            re =  Request(url=url, callback=self.parse_item)
+            re.meta["proxy"] = self.CUSTOM_PROXY
+
+            yield re"""
 
 
     def parse_item(self, response):
