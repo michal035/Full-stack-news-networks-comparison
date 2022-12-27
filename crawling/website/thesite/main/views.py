e33e268ab861735cde213bd2a3f5f34d095d2d46
Michal <mwiselka035@gmail.com>
2022-12-27 16:30:54 +0100

    2 languages - user can choose what language the content is displayed in

--- crawling/website/thesite/main/views.py
+++ crawling/website/thesite/main/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse, response
 from django.http import HttpResponse, HttpResponseRedirect
 
-
 def index(response):
     #return  HttpResponse("<h1>Hello </h1>")
-    return render(response, "main/index.html", {})
+
+    print(response.method)
+    if response.method == 'POST':
+        
+        lng = "Eng"
+
+        if response.POST.get("btn1"):
+            pass
+        elif response.POST.get("btn2"):
+            lng = "Pol"
+        
+
+
+
+    return render(response, "main/index.html", {"lng":lng})
