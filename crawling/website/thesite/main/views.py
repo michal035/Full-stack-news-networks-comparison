fb1c749132c3e7526137c900fd419b2841d08626
Michal <mwiselka035@gmail.com>
2023-01-01 17:42:33 +0100

    some progress i am able to display data from one table at the time into html table

--- crawling/website/thesite/main/views.py
+++ crawling/website/thesite/main/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse, response
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import um
+from .models import tvn,tvp, um
+
+
+class merge():
+
+    def __init__(self,a="",b="") -> None:
+        self.tvn = a
+        self.tvp = b
+    
+    def __repr__(self) -> str:
+        return f"tvn = {self.tvn} tvp={self.tvp}"
 
 
 def index(response):
 
+
     lng = "Eng"
+   
     print(response.method)
     if response.method == 'POST':
         
@@ -15,9 +27,48 @@ def index(response):
             pass
         elif response.POST.get("btn2"):
             lng = "Pol"
-        
 
-    return render(response, "main/index.html", {"lng":lng})
+
+    tvP = tvp.objects.all()
+    tvN = tvn.objects.all()
+
+
+    
+    """objs = []
+
+
+    #whole merge thing might need to beredone
+    d = 0
+    dd = 0
+    the_thing = 0
+
+
+    if len(tvP) > len(tvN):
+        d = len(tvP)
+    else:
+        d = len(tvN)
+        the_thing = 1
+
+    for i in range(d):
+        if the_thing == 1:
+            obj = merge(tvN[i].headline)
+            objs.append(obj)
+        else:
+            obj = merge(tvN[i].headline)
+            objs.append(obj)
+
+
+    for i in range(dd):
+        if the_thing == 0:
+            objs[i].tvn = 
+    """
+
+
+
+    context = {'tvp': tvP,'tvn': tvN ,"lng":lng, }
+
+    return render(response, "main/index.html", context )
+ 
 
 
 def db_test(resposne):
@@ -25,7 +76,7 @@ def db_test(resposne):
     print(obj)
     context = {'obj':obj}
     return render(resposne, "main/dbtest.html", {"c":context})"""
-    p = um.objects.all()
+    p = tvp.objects.all()
     context = {'p': p,}
     return render(resposne, "main/dbtest.html", context)
 
