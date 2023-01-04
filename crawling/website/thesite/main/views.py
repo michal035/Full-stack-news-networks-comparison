1ea83965f09ce05dc9453a9625d7f1da1a74af79
Michal <mwiselka035@gmail.com>
2023-01-04 23:59:54 +0100

    a lot

--- crawling/website/thesite/main/views.py
+++ crawling/website/thesite/main/views.py
@@ -25,8 +25,44 @@ class merge():
         return f"tvn = {self.tvn} tvp={self.tvp}"
 
 
-def index(response):
+def adding_stuff_to_merge(tvP,tvN):
+    objs = []
 
+
+    #whole merge thing needs to be rewritten
+    d = 0
+    dd = 0
+    the_thing = 0
+
+
+    if len(tvP) > len(tvN):
+        d = len(tvP)
+        dd = len(tvN)
+    else:
+        d = len(tvN)
+        dd = len(tvP)
+        the_thing = 1
+
+    for i in range(d):
+        #here needs to be added that blank or so
+        if the_thing == 1:
+            obj = merge(a=tvN[i].headline)
+            objs.append(obj)
+        else:
+            obj = merge(b=tvP[i].headline)
+            objs.append(obj)
+
+
+    for i in range(dd):
+        if the_thing == 1: 
+            objs[i].add_other("tvp",tvP[i].headline)
+        else:
+            objs[i].add_other("tvn",tvN[i].headline)
+    
+    return objs
+
+
+def index(response):
 
     lng = "Eng"
    
@@ -51,41 +87,7 @@ def index(response):
 
 
 
-
-    objs = []
-
-
-    #whole merge thing needs to be rewritten
-    d = 0
-    dd = 0
-    the_thing = 0
-
-
-    if len(tvP) > len(tvN):
-        d = len(tvP)
-        dd = len(tvN)
-    else:
-        d = len(tvN)
-        dd = len(tvP)
-        the_thing = 1
-
-    for i in range(d):
-        if the_thing == 1:
-            obj = merge(a=tvN[i].headline)
-            objs.append(obj)
-        else:
-            obj = merge(b=tvP[i].headline)
-            objs.append(obj)
-
-
-    for i in range(dd):
-        if the_thing == 1: 
-            objs[i].add_other("tvp",tvP[i].headline)
-        else:
-            objs[i].add_other("tvn",tvN[i].headline)
-    
-
-
+    objs = adding_stuff_to_merge(tvP,tvN)
     context = {'hl': objs,"lng":lng, }
     #context = {'tvp': tvP,'tvn': tvN ,"lng":lng, }
 
@@ -93,8 +95,66 @@ def index(response):
  
 
 def archive(response, number_of_days=1):
-    return render(response, "main/archive.html",{'l':number_of_days} )
+    
+    t = datetime.now()
+    
+    date = t.strftime("%Y-%m-%d")
+    
+    
+    """day = t.day
+    month = t.month
+    year = t.year
+
+
+    for i in range(int(number_of_days)):
+        date = f"{year}-{month}-{day}"
+        tvP = tvp.objects.raw(f"select * from main_tvp where date = '{date}'")
+        tvN = tvn.objects.raw(f"select * from main_tvn where date = '{date}'")
+        
+        
+        if day != 1 :
+            day -= 1
+        else:
+            month -=1"""
+    # 1 -> 2
+
+    day = t.day
+    month = t.month
+    year = t.year
+
+    eh = 1
+    
+
+    list_of_objs = []
+    while  eh <= int(number_of_days):
+
+        date = f"{year}-{month}-{28}" #
+        
+        #date2 = f"{year}-{month}-{1}"
+        if month == 1:
+            date2 = f"{year-1}-12-{1}"
+        else:
+            date2 = f"{year}-{month-1}-{1}" 
+        
+        print(f"date2 - {date2} date - {date}")
+        tvP = tvp.objects.raw(f"select * from main_tvp where date between '{date2}' and '{date}'")
+        tvN = tvn.objects.raw(f"select * from main_tvn where date between '{date2}' and '{date}'")
+
+
+        if month == 1:
+            year -= 1
+            month = 11
+        else:
+            month -= 2
+
+        objs = adding_stuff_to_merge(tvP,tvN)
+        list_of_objs.append(objs)
+        eh +=1
+    
+    return render(response, "main/archive.html",{'l': list_of_objs} )
+    #return render(response, "main/archive.html",{'l':number_of_days} )
  
+
 
 
 
