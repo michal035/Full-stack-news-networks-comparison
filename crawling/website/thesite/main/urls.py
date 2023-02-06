b7160ebefc3d55942f6151ace63359f32587a127
Michal <mwiselka035@gmail.com>
2023-02-06 16:12:40 +0100

    hotfix

--- crawling/website/thesite/main/urls.py
+++ crawling/website/thesite/main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('index/', views.index2),
     path('archive/', views.archive),
     path('archive/<keyword>/', views.archive),
-    path('key words/', views.key_words),
+    path('keywords/', views.key_words),
     path('api/', views.api),
     path('api/<keyword>/', views.api)
  
